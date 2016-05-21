bd6bb005f6e4f14402fa2e5cab7e55d68b0fb228
Akshay Kumar <akshay.kesarwan@flipkart.com>
2016-05-22 01:14:18 +0530

    Refined controller

--- src/main/java/com/fast/prezi/resources/PresentationController.java
+++ src/main/java/com/fast/prezi/resources/PresentationController.java
@@ -1,18 +1,21 @@
 package com.fast.prezi.resources;
 
 import com.fast.prezi.core.services.interfaces.FastPreziService;
+import com.fast.prezi.data.model.Presentation;
 import com.google.inject.Inject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * Created by akshay.kesarwan on 21/05/16.
  */
-@Path("/ppt")
+@Path("/presentations")
 @Produces(MediaType.APPLICATION_JSON)
 public class PresentationController {
 
@@ -28,5 +31,27 @@ public class PresentationController {
     public Response test() {
         String result = "Testing Successful";
         return Response.status(Response.Status.OK).entity(result).build();
+    }
+
+    @GET
+    @Path("/")
+    public Response getAllImages(@QueryParam("page_number") Integer pageNumber, @QueryParam("page_size") Integer pageSize) {
+        List<Presentation> presentations = fastPreziService.getPresentations(pageNumber, pageSize);
+        return Response.status(Response.Status.OK).entity(presentations).build();
+    }
+
+    @GET
+    @Path("/sort")
+    public Response getSortedImages(@QueryParam("page_number") Integer pageNumber, @QueryParam("page_size") Integer pageSize) {
+        List<Presentation> presentations = fastPreziService.getSortedPresentations(pageNumber, pageSize);
+        return Response.status(Response.Status.OK).entity(presentations).build();
+    }
+
+    @GET
+    @Path("/search")
+    public Response searchPresentations(@QueryParam("title") String title, @QueryParam("page_number") Integer pageNumber,
+                                        @QueryParam("page_size") Integer pageSize) {
+        List<Presentation> presentations = fastPreziService.searchPresentations(title, pageNumber, pageSize);
+        return Response.status(Response.Status.OK).entity(presentations).build();
     }
 }
