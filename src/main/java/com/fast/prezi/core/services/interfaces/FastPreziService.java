7b2238ecff411262571163060f4e435dd78e270b
Akshay Kumar <akshay.kesarwan@flipkart.com>
2016-05-22 01:11:38 +0530

    Defined service interface

--- src/main/java/com/fast/prezi/core/services/interfaces/FastPreziService.java
+++ src/main/java/com/fast/prezi/core/services/interfaces/FastPreziService.java
@@ -1,7 +1,14 @@
 package com.fast.prezi.core.services.interfaces;
 
+import com.fast.prezi.data.model.Presentation;
+
+import java.util.List;
+
 /**
  * Created by akshay.kesarwan on 21/05/16.
  */
 public interface FastPreziService {
+    List<Presentation> getPresentations(Integer pageNumber, Integer pageSize);
+    List<Presentation> getSortedPresentations(Integer pageNumber, Integer pageSize);
+    List<Presentation> searchPresentations(String titleRegex, Integer pageNumber, Integer pageSize);
 }
