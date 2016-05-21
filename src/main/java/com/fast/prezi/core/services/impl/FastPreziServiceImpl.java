b2b1409f5c9175162d97b407c81891b93a498c8b
Akshay Kumar <akshay.kesarwan@flipkart.com>
2016-05-22 01:12:37 +0530

    Service Implementation

--- src/main/java/com/fast/prezi/core/services/impl/FastPreziServiceImpl.java
+++ src/main/java/com/fast/prezi/core/services/impl/FastPreziServiceImpl.java
@@ -1,17 +1,43 @@
 package com.fast.prezi.core.services.impl;
 
-import com.fast.prezi.core.configs.JsonDaoConfig;
 import com.fast.prezi.core.services.interfaces.FastPreziService;
+import com.fast.prezi.data.dao.interfaces.PresentationDao;
+import com.fast.prezi.data.model.Presentation;
 import com.google.inject.Inject;
+
+import java.util.List;
 
 /**
  * Created by akshay.kesarwan on 21/05/16.
  */
 public class FastPreziServiceImpl implements FastPreziService {
-    private final JsonDaoConfig jsonDaoConfig;
+    private final PresentationDao presentationDao;
+    private final Integer DEFAULT_PAGE_SIZE = 20;
+    private final Integer DEFAULT_PAGE_NUMBER = 0;
 
     @Inject
-    FastPreziServiceImpl(JsonDaoConfig jsonDaoConfig) {
-        this.jsonDaoConfig = jsonDaoConfig;
+    FastPreziServiceImpl(PresentationDao presentationDao) {
+        this.presentationDao = presentationDao;
+    }
+
+    @Override
+    public List<Presentation> getPresentations(Integer pageNumber, Integer pageSize) {
+        pageNumber = pageNumber!=null?pageNumber:DEFAULT_PAGE_NUMBER;
+        pageSize = pageSize!=null?pageSize:DEFAULT_PAGE_SIZE;
+        return presentationDao.getPresentations(pageNumber, pageSize);
+    }
+
+    @Override
+    public List<Presentation> getSortedPresentations(Integer pageNumber, Integer pageSize) {
+        pageNumber = pageNumber!=null?pageNumber:DEFAULT_PAGE_NUMBER;
+        pageSize = pageSize!=null?pageSize:DEFAULT_PAGE_SIZE;
+        return presentationDao.getSortedPresentations(pageNumber, pageSize);
+    }
+
+    @Override
+    public List<Presentation> searchPresentations(String titleRegex, Integer pageNumber, Integer pageSize) {
+        pageNumber = pageNumber!=null?pageNumber:DEFAULT_PAGE_NUMBER;
+        pageSize = pageSize!=null?pageSize:DEFAULT_PAGE_SIZE;
+        return presentationDao.searchPresentations(titleRegex, pageNumber, pageSize);
     }
 }
