41ca46b28df200b206ddb66937bed785b43e0b8f
Akshay Kumar <akshay.kesarwan@flipkart.com>
2016-05-23 01:48:08 +0530

    Added CORS support

--- src/main/java/com/fast/prezi/FastPreziApplication.java
+++ src/main/java/com/fast/prezi/FastPreziApplication.java
@@ -5,6 +5,11 @@ import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 /**
  * Created by akshay.kesarwan on 21/05/16.
@@ -26,6 +31,14 @@ public class FastPreziApplication extends Application<FastPreziConfiguration> {
     @Override
     public void run(FastPreziConfiguration configuration, Environment environment) throws Exception {
         environment.healthChecks().register("FastPrezi", new FastPreziHealthCheck());
+        // Add CORS Filter
+        FilterRegistration.Dynamic filter =environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
     }
 
     public static void main(String args[]) throws Exception {
